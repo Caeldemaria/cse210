c64fe253cd05892241b77997e4ef0afd27ab2ed1
Nathasha Alice <demariamicael@gmail.com>
2025-01-18 14:56:34 -0300

    Exercise 2 is finished

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Waht is the grade percentage?");
+        string Answer=Console.ReadLine();
+
+        int percentage= int.Parse(Answer);
+        string letter=" ";
+        if(percentage>=90){
+            letter="A";
+            Console.WriteLine(letter);
+        }
+        else if(percentage>=80 && percentage<90){
+        letter="B";
+            Console.WriteLine(letter);   
+        }
+        else if(percentage>=70 && percentage<80){
+        letter="C";
+            Console.WriteLine(letter);   
+        }
+        else if(percentage>=60 && percentage<70){
+        letter="D";
+            Console.WriteLine(letter);    
+        }
+        else{
+        letter="F";
+            Console.WriteLine(letter);    
+        };
+        if (percentage>=70){
+            Console.WriteLine("Congratulations you passed");
+        }
+        else{
+            Console.WriteLine("Don't worry you can be better in the next time!");
+        }
+
+
     }
 }

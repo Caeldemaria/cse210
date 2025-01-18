2487b3b2774906a465f8714af5fe99e07035bf7a
Nathasha Alice <demariamicael@gmail.com>
2025-01-17 22:49:06 -0300

    Exercise 1 finish

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name?");
+        string first_Name= Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string Last_Name=Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {Last_Name},{first_Name} {Last_Name}");
+        
     }
 }

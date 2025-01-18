06c82ae3891005965b86b7ca6684b674affe538f
Nathasha Alice <demariamicael@gmail.com>
2025-01-18 19:46:12 -0300

    Exercise 4 is Finished

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers=new List<int>();
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int NewNumber=-1;
+        int sum=0;
+        int Avarege=0;
+        int  largest=int.MinValue;
+        while(NewNumber!=0){
+            Console.WriteLine("Enter number: ");
+            NewNumber=int.Parse(Console.ReadLine());
+            if(NewNumber!=0){
+                numbers.Add(NewNumber);
+            }
+            else{
+                foreach (int number in numbers)
+                {
+                    if(number>largest){
+                        largest=number;
+                    };
+                    sum+=number;
+
+                    
+                };
+                Avarege=sum/numbers.Count;
+                Console.WriteLine($"The sum is: {sum}");
+                Console.WriteLine($"The Avarege is: {Avarege}");
+                Console.WriteLine($"The largest number is: {largest}");
+            }
+        }
     }
 }
